44b6c8eb9f620f432ceb36b70d71557bd611939b
rsadevops <134621697+rsadevops@users.noreply.github.com>
2023-06-16 12:00:53 +0300

    Create generate_version.sh

---
+++ scripts/generate_version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Set Git user identity
+git config --global user.email "autogenver@tech.io"
+git config --global user.name "autogenerate version"
+
+
+#Determine version
+commit_message=$(git log --format=%s -n 1)
+
+if echo "$commit_message" | grep -qE '^major:'; then
+  new_version=$(npm --no-git-tag-version version major --allow-same-version)
+
+elif echo "$commit_message" | grep -qE '^minor:'; then
+  new_version=$(npm --no-git-tag-version version minor --allow-same-version)
+
+elif echo "$commit_message" | grep -qE '^patch:'; then
+  new_version=$(npm --no-git-tag-version version patch --allow-same-version)
+
+elif echo "$commit_message" | grep -qE '^manual:'; then
+  new_version=$(node -p "require('./package.json').version")
+else
+  new_version=$(npm --no-git-tag-version version prerelease --allow-same-version)
+fi
+
+#Update package.json
+echo "new_version=$new_version" >> $GITHUB_OUTPUT
+
+npm --no-git-tag-version version $new_version --allow-same-version
+
+git add package.json
+git commit -m "Bump version to $new_version"
+git push --quiet
