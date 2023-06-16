b11ecc06684c869f8fd83c08e1a6eb473e230de1
rsadevops <raman.sauchuk@devstream.by>
2023-06-16 16:29:21 +0300

    prerelease: dsada

--- scripts/generate_version.sh
+++ scripts/generate_version.sh
@@ -27,7 +27,6 @@ echo "new_version=$new_version" >> $GITHUB_OUTPUT
 
 npm --no-git-tag-version version $new_version --allow-same-version
 
-git remote set-url origin https://${GITHUB_TOKEN}github.com/rsadevops/hulitinerabotaesh.git
 git add package.json
 git commit -m "Bump version to $new_version"
 git push --quiet
