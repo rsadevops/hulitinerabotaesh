190d5c816de9fb3e20d000e436c2b5ce4392d384
rsadevops <134621697+rsadevops@users.noreply.github.com>
2023-06-16 14:33:44 +0300

    Update generate_version.sh

--- scripts/generate_version.sh
+++ scripts/generate_version.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-GITHUB_ACTOR=${1}
-
 #Set Git user identity
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --global user.name "${GITHUB_ACTOR}"
